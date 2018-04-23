5fb06c52a868a6901048fd491e0394f6fd2b4337
Health Guide <admin@healthguide.com>
2018-04-23 16:34:49 -0700

    fixed an issue where yoast cannical removal was not triggering properly

--- includes/clone-posts-canonical.php
+++ includes/clone-posts-canonical.php
@@ -5,9 +5,6 @@
 * No return
 */
 
-//disable default canonical tag insertion
-remove_action('wp_head', 'rel_canonical');
-
 //generate new canonical reference to the original post
 function clone_post_canonical_link() {
   if (!is_single())
@@ -17,13 +14,8 @@ function clone_post_canonical_link() {
   //store canonical reference
   $canonical_reference = get_post_meta( get_the_ID(), 'canonical_reference', true);
   //if canonical reference exists and is not empty
-  if ($canonical_reference && !empty($canonical_reference))
+  if (!empty($canonical_reference))
   {
-    //if yoast is already generating a canonical url, disable the filter
- if ( function_exists( 'wpseo_canonical' ) ) {
-      add_filter( 'wpseo_canonical', '__return_false' );
-    }
-
     //get the post permalink of the canonical reference
     $canonical_url = get_permalink($canonical_reference);
     if ($page > 1) {
@@ -31,9 +23,22 @@ function clone_post_canonical_link() {
     }
     //echo that statement
     echo '<link rel="canonical" href="'. $canonical_url .'" />';
-  } else {
-    //add back default canonical tag insertion
-    add_action('wp_head', 'rel_canonical', 100);
   }
 }
-add_action('wp_head', 'clone_post_canonical_link', 10);
+add_action('wp_head', 'clone_post_canonical_link', 4);
+
+
+//method to remove yoast & default canonical links
+function remove_canonical() {
+  //store canonical reference
+  $canonical_reference = get_post_meta( get_the_ID(), 'canonical_reference', true);
+  //if canonical reference exists and is not empty
+  if (!empty($canonical_reference))
+  {
+    //if yoast is already generating a canonical url, disable the filter
+    add_filter( 'wpseo_canonical', '__return_false', 10);
+    //disable default canonical tag insertion
+    remove_action('wp_head', 'rel_canonical');
+  }
+}
+add_action( 'wpseo_head', 'remove_canonical', 4);
